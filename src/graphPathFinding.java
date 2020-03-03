fb01bae311aef068f620d606ee03b00249e96d12
ehuan2 <47696422+ehuan2@users.noreply.github.com>
2020-03-03 10:07:55 -0500

    Update graphPathFinding.java

--- src/graphPathFinding.java
+++ src/graphPathFinding.java
@@ -2,9 +2,29 @@
 public class graphPathFinding {
 
   static int[] retracePath;
+  static HashMap<String, Boolean> flagMap;
   
-  public static void findPath(int[]xValues, int[]yValues, int[]radius){
+  public static void findPath(int xLocation, int yLocation, int xDest, int yDest, int[]xValues, int[]yValues, int[]radius){ // BFS for path
   
+    Queue<Integer> qX = new LinkedList<>(); // queues needed, each for x coordinate and y coordinate
+    Queue<Integer> qY = new LinkedList<>();
+    
+    qX.add(xLocation); // adds starting ones
+    qY.add(yLocation);
+    
+    flagMap = new HashMap<>(); // flags the ones down
+    
+    while(!qX.isEmpty() && !qY.isEmpty()){
+      
+      int xNext = qX.poll();
+      int yNext = qY.poll();
+      
+      
+      
+      
+      
+    }
+    
   
   }
   
