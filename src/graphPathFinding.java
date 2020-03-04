9a696401adc29922c815b7295775c2a618484793
ehuan2 <47696422+ehuan2@users.noreply.github.com>
2020-03-04 11:11:21 -0500

    Update graphPathFinding.java

--- src/graphPathFinding.java
+++ src/graphPathFinding.java
@@ -44,7 +44,11 @@ public class graphPathFinding {
   static HashMap<String, Boolean> flagMap;
   
   public static pointLocation[] findPath(int xLocation, int yLocation, int xDest, int yDest, int[]xValues, int[]yValues, int[]radius){ // BFS for path
-  
+   
+   if(inContaminationAreas(xLocation, yLocation, xValues, yValues, radius){
+    return null;
+   }
+   
     Queue<Integer> qX = new LinkedList<>(); // queues needed, each for x coordinate and y coordinate
     Queue<Integer> qY = new LinkedList<>();
     
@@ -66,13 +70,31 @@ public class graphPathFinding {
       
       
     }
+   
+   return null;
     
   
   }
  
  
- 
+ public static boolean withinRadius(int xLocale, int yLocale, int xRadius, int yRadius, int radius){ // returns true if in radius
   
+  return Math.pow((xLocale-xRadius), 2) + Math.pow((yLocale - yRadius), 2) <= Math.pow(radius, 2);
+  
+ }
 
 
+ public static boolean inContaminationAreas(int xLocation, int yLocation, int[]xValues, int[]yValues, int[]radius){
+  int numContamination = xValues.length;
+   
+   for(int i = 0; i < numContamination; i++){
+     if(withinRadius(xLocation, yLocation, xValues[i], yValues[i], radius[i])){
+      return true;
+     }
+   }
+  
+  return false;
+  
+ }
+ 
 }
