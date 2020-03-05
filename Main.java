3171394d9d637d1858cc1c56015b9736e85d481c
aidanli22 <60661800+aidanli22@users.noreply.github.com>
2020-03-05 10:19:02 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.net.URL;
+import javax.sound.sampled.*;
+import javax.swing.*;
+
+// To play sound using Clip, the process need to be alive.
+// Hence, we use a Swing application.
+public class Main extends JFrame {
+
+   public Main() {
+      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      this.setTitle("Test Sound Clip");
+      this.setSize(300, 200);
+      this.setVisible(true);
+
+      try {
+         // Open an audio input stream.
+         URL url = this.getClass().getClassLoader().getResource("OmaeWaMouShineiru.wav");
+         AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);
+         // Get a sound clip resource.
+         Clip clip = AudioSystem.getClip();
+         // Open audio clip and load samples from the audio input stream.
+         clip.open(audioIn);
+         clip.start();
+      } catch (UnsupportedAudioFileException e) {
+         e.printStackTrace();
+      } catch (IOException e) {
+         e.printStackTrace();
+      } catch (LineUnavailableException e) {
+         e.printStackTrace();
+      }
+   }
+
+   public static void main(String[] args) {
+      new Main();
+   }
+}
